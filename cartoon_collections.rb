01f5ef0c6a5eec3a9f2d0b8dcb3798ce2be36319
Vanna Winland <vgwinland@gmail.com>
2019-09-17 19:01:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,8 @@ end
 
 
 def long_planeteer_calls(words)
-  words.any? do |words|  
+    words.any? do |words|  words > 4                            
+  end
 end
 
 def find_the_cheese# code an argument here
